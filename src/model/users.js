56a289cf82e71f3a1f3b1fa45e8ae0eced78e314
Yeskendir <yd.kassenov@gmail.com>
2021-11-03 17:12:06 +0600

    test users: change username back to Adam, b/c apple testFlight might have memorized the name (can't update in UI); ios: bump build number to 5

--- src/model/users.js
+++ src/model/users.js
@@ -2,13 +2,13 @@
 const Users = [
   {
     id: 1,
-    email: 'anders@email.com',
-    username: 'anders',
+    email: 'adam@email.com',
+    username: 'adam',
     password: 'qwerty',
     userToken: 'token123',
     familyName: 'Allison',
     givenName: 'A',
-    name: 'Anders',
+    name: 'Adam',
     photoUrl:
       'https://images.generated.photos/6nIN56AWpT7A4UafqanZ48-M7f5ZwfvyQJpVR3qEDjE/rs:fit:512:512/wm:0.95:sowe:18:18:0.33/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/MDc0MDk5LmpwZw.jpg',
     hhToken:
