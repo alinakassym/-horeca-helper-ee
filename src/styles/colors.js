38cef9287f06ad4b44b3c2922314c8129247340f
Alina <alinakassym@gmail.com>
2021-12-14 00:32:18 +0600

    add global color & rm unused styles

---
+++ src/styles/colors.js
@@ -0,0 +1,15 @@
+export const PrimaryColors = {
+  brand: '#2A8BE4',
+  element: '#151F47',
+  grey1: '#8391A1',
+  grey2: '#B9C1CA',
+  grey3: '#E2E5E8',
+  grey4: '#F0F2F4',
+  background: '#F5F8FE',
+  white: '#FFFFFF',
+};
+
+export const StatusesColors = {
+  green: '#40C14D',
+  red: '#EC4C47',
+};
--- src/styles/globalStyles.js
+++ src/styles/globalStyles.js
@@ -1,78 +1,18 @@
-import {StyleSheet} from 'react-native';
-import {Platform} from 'react-native';
+import {Dimensions, StyleSheet} from 'react-native';
+import {PrimaryColors} from './colors';
+
+const dimensions = Dimensions.get('screen');
 
 export const globalStyles = StyleSheet.create({
+  fullScreenSection: {flex: 1, justifyContent: 'center', alignItems: 'center'},
   container: {
     flex: 1,
-    backgroundColor: '#FFFFFF',
+    backgroundColor: PrimaryColors.background,
   },
-  modalTopBar: {
-    marginTop: Platform.OS === 'ios' ? 40 : 0,
+  card: {
+    marginTop: 8,
+    padding: 20,
+    width: dimensions.width,
+    backgroundColor: PrimaryColors.white,
   },
-  label: {
-    marginBottom: 8,
-    fontFamily: 'Roboto-Medium',
-    fontSize: 16,
-  },
-  primaryInput: {
-    marginBottom: 16,
-    paddingVertical: 8,
-    paddingHorizontal: 16,
-    fontFamily: 'Roboto-Regular',
-    fontSize: 16,
-    color: '#000000',
-    borderRadius: 8,
-    borderWidth: 1,
-    borderColor: '#CCCCCC',
-  },
-  multiline: {
-    height: 100,
-    textAlignVertical: 'top',
-  },
-  text: {
-    color: '#000000',
-  },
-  select: {
-    marginBottom: 16,
-    paddingVertical: 12,
-    paddingHorizontal: 16,
-    fontFamily: 'Roboto-Regular',
-    fontSize: 16,
-    color: '#000000',
-    borderRadius: 8,
-    borderWidth: 1,
-    borderColor: '#CCCCCC',
-  },
-
-  // --- start of Filters TopBar --- //
-  topBar: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    paddingVertical: 10,
-    paddingHorizontal: 12,
-    backgroundColor: '#F6F6F6',
-  },
-  filterBtn: {
-    position: 'relative',
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  filterApplied: {
-    position: 'absolute',
-    top: 1,
-    left: 22,
-    height: 10,
-    width: 10,
-    borderRadius: 5,
-    backgroundColor: '#E74C3C',
-  },
-  filterBtnRightText: {
-    marginLeft: 6,
-    fontSize: 14,
-  },
-  filterBtnLeftText: {
-    marginRight: 4,
-    fontSize: 14,
-  },
-  // --- end of Filters TopBar --- //
 });
