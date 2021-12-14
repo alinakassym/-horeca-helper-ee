e67be82bdf486bba3437359ae505f6181b2f6f02
Alina <alinakassym@gmail.com>
2021-12-14 15:58:02 +0600

    add icons ExpandRight, SignOut

--- src/assets/icons/main/IconExpandRight.js
+++ src/assets/icons/main/IconExpandRight.js
@@ -1,12 +1,15 @@
 import * as React from 'react';
 import Svg, {Path} from 'react-native-svg';
 
-export const IconExpandRight = ({color, size}) => {
+export const IconExpandRight = ({size, color, width}) => {
   return (
-    <Svg width={size || 24} height={size || 24} viewBox="0 0 24 24" fill="none">
+    <Svg width={size || 24} height={size || 24} fill="none">
       <Path
-        d="M15 12l.707-.707.707.707-.707.707L15 12zM9.707 5.293l6 6-1.414 1.414-6-6 1.414-1.414zm6 7.414l-6 6-1.414-1.414 6-6 1.414 1.414z"
-        fill={color || '#000000'}
+        d="m6 3 5 5-5 5"
+        stroke={color || '#000000'}
+        strokeWidth={width || 2}
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </Svg>
   );
