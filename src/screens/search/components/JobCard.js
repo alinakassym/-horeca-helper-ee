87b54a29c7c00546ae5700dcfa341de1061bd760
Alina <alinakassym@gmail.com>
2021-12-23 15:01:54 +0600

    feat: Job card add updatedAt

--- src/screens/search/components/JobCard.js
+++ src/screens/search/components/JobCard.js
@@ -7,6 +7,8 @@ import {
   Image,
   Dimensions,
 } from 'react-native';
+import moment from 'moment';
+import 'moment/locale/ru';
 
 // styles
 import {globalStyles} from '../../../styles/globalStyles';
@@ -32,6 +34,8 @@ export const JobCard = ({item, onPress}) => {
     return parts.join('.');
   };
 
+  const formatDate = date => moment(date).locale('ru').format('DD MMM YYYY');
+
   const getSalary = (salaryMin, salaryMax) => {
     if (salaryMin && salaryMax) {
       return `${numberWithSpaces(salaryMin)} - ${numberWithSpaces(salaryMax)}`;
@@ -41,56 +45,63 @@ export const JobCard = ({item, onPress}) => {
     return `до ${numberWithSpaces(salaryMax)}`;
   };
   return (
-    <Pressable style={globalStyles.card} onPress={onPress}>
-      <View style={[styles.row, styles.mb2]}>
-        <View style={styles.leftCol}>
-          <View style={styles.row}>
-            <Text style={styles.positionTitle}>{item?.position?.title_ru}</Text>
-            <SmallBadge
-              style={{marginLeft: 8}}
-              text={'TOP'}
-              color={'#9B51E0'}
-            />
+    <React.Fragment>
+      <Pressable style={globalStyles.card} onPress={onPress}>
+        <View style={[styles.row, styles.mb2]}>
+          <View style={styles.leftCol}>
+            <View style={styles.row}>
+              <Text style={styles.positionTitle}>
+                {item?.position?.title_ru}
+              </Text>
+              <SmallBadge
+                style={styles.smallBadge}
+                text={'TOP'}
+                color={'#9B51E0'}
+              />
+            </View>
+            <View style={[styles.row, styles.alignCenter]}>
+              <IconBuilding color={PrimaryColors.grey1} size={16} />
+              <Text style={[styles.text, styles.ml]}>{item.company.title}</Text>
+            </View>
+            <View style={[styles.row, styles.alignCenter]}>
+              <IconAddress color={PrimaryColors.grey1} size={16} />
+              <Text numberOfLines={1} style={[styles.text, styles.ml]}>
+                {item.company.address}
+              </Text>
+            </View>
           </View>
-          <View style={[styles.row, styles.alignCenter]}>
-            <IconBuilding color={PrimaryColors.grey1} size={16} />
-            <Text style={[styles.text, styles.ml]}>{item.company.title}</Text>
-          </View>
-          <View style={[styles.row, styles.alignCenter]}>
-            <IconAddress color={PrimaryColors.grey1} size={16} />
-            <Text numberOfLines={1} style={[styles.text, styles.ml]}>
-              {item.company.address}
-            </Text>
+          <View style={styles.imageWrapper}>
+            <Image style={styles.image} source={{uri: item.company.photoUrl}} />
+            {item.isActive && <View style={styles.indicator} />}
           </View>
         </View>
-        <View style={styles.imageWrapper}>
-          <Image style={styles.image} source={{uri: item.company.photoUrl}} />
-          {item.isActive && <View style={styles.indicator} />}
+        <View
+          style={[
+            styles.row,
+            styles.alignCenter,
+            styles.justifyBetween,
+            styles.mb,
+          ]}>
+          <Text style={styles.salary}>
+            {getSalary(item.salaryMin, item.salaryMax)} ₸
+          </Text>
+          <RatingScale score={Math.ceil(item.company.avgAvgScore)} />
         </View>
-      </View>
-      <View
-        style={[
-          styles.row,
-          styles.alignCenter,
-          styles.justifyBetween,
-          styles.mb,
-        ]}>
-        <Text style={styles.salary}>
-          {getSalary(item.salaryMin, item.salaryMax)} ₸
-        </Text>
-        <RatingScale score={Math.ceil(item.company.avgAvgScore)} />
-      </View>
-      <Text style={[styles.text, styles.mb2]}>{item.description}</Text>
-      <View style={[styles.row]}>
-        <GradientButton style={{width: width - 196}} label={'Откликнуться'} />
-        <OutlineButton
-          style={{marginLeft: 8, width: 148}}
-          labelStyle={{marginLeft: 4}}
-          label={'В избранные'}>
-          <IconBookmark width={1.7} size={16} color={PrimaryColors.brand} />
-        </OutlineButton>
-      </View>
-    </Pressable>
+        <Text style={[styles.text, styles.mb2]}>{item.description}</Text>
+        <View style={[styles.row]}>
+          <GradientButton style={{width: width - 196}} label={'Откликнуться'} />
+          <OutlineButton
+            style={styles.outlineBtn}
+            labelStyle={styles.outlineBtnLabel}
+            label={'В избранные'}>
+            <IconBookmark width={1.7} size={16} color={PrimaryColors.brand} />
+          </OutlineButton>
+        </View>
+      </Pressable>
+      <Text style={styles.updatedAt}>
+        Обновлено {formatDate(item.updatedAt)}
+      </Text>
+    </React.Fragment>
   );
 };
 
@@ -154,4 +165,16 @@ const styles = StyleSheet.create({
     lineHeight: 18,
     color: PrimaryColors.grey1,
   },
+  updatedAt: {
+    padding: 8,
+    fontFamily: 'Inter-Regular',
+    fontSize: 13,
+    lineHeight: 16,
+    textAlign: 'center',
+    color: PrimaryColors.grey1,
+    backgroundColor: PrimaryColors.grey4,
+  },
+  smallBadge: {marginLeft: 8},
+  outlineBtn: {marginLeft: 8, width: 148},
+  outlineBtnLabel: {marginLeft: 4},
 });
